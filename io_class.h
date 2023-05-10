362adc3cfe5457165ee7a5316a2941137b3e1d9f
Lin-jinren <jinrenlin16@gmail.com>
2023-05-10 12:20:13 +0800

    conmit message

--- io_class.h
+++ io_class.h
@@ -8,6 +8,25 @@ private:
     float weight;
     float height;
 public:
-    Student(/* args */);
+    char name[20];
+    int id;
+    char class_name[20];
+    float grade[5];
+    int g_len;
+
+    float turn_cm_to_m(float);
+    float get_BMI();
+    float get_BMI(float, float);
+    float get_grade_avg();
+
+    void set_weight(float);
+    void set_height(float);
+    void set_public_info();
+    void print_grade();
+
+    Student();
     ~Student();
+    Student(float g[], int len);
+    Student(float w, float h) :weight(w), height(h) {g_len = 0, id = 0;}
+    
 };
--- io_data.cpp
+++ io_data.cpp
@@ -1,9 +1,44 @@
 #include "io_class.h"
 
-Student::Student(/* args */)
+Student::Student()
 {
 }
 
 Student::~Student()
 {
+}
+
+Student::Student(float g[], int len){
+    
+}
+
+float Student::turn_cm_to_m(float){
+
+}
+float Student::get_BMI(){
+
+}
+
+float Student::get_BMI(float, float){
+
+}
+
+float Student::get_grade_avg(){
+
+}
+
+void Student::set_weight(float){
+
+}
+
+void Student::set_height(float){
+
+}
+
+void Student::set_public_info(){
+
+}
+
+void Student::print_grade(){
+
 }
